34df011dcbc189867c3d8c700fc989cb80da8c50
hilal <130781977+mshelovee@users.noreply.github.com>
2023-11-20 10:44:09 +0300

    Add files via upload

---
+++ listbox random sayi -96/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace listbox_random_sayi__96
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbSayilar = new System.Windows.Forms.ListBox();
+            this.txtSayi = new System.Windows.Forms.TextBox();
+            this.btnAra = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbSayilar
+            // 
+            this.lbSayilar.FormattingEnabled = true;
+            this.lbSayilar.ItemHeight = 16;
+            this.lbSayilar.Location = new System.Drawing.Point(23, 71);
+            this.lbSayilar.Margin = new System.Windows.Forms.Padding(4);
+            this.lbSayilar.Name = "lbSayilar";
+            this.lbSayilar.Size = new System.Drawing.Size(201, 340);
+            this.lbSayilar.TabIndex = 0;
+            // 
+            // txtSayi
+            // 
+            this.txtSayi.Location = new System.Drawing.Point(23, 30);
+            this.txtSayi.Margin = new System.Windows.Forms.Padding(4);
+            this.txtSayi.Name = "txtSayi";
+            this.txtSayi.Size = new System.Drawing.Size(150, 22);
+            this.txtSayi.TabIndex = 1;
+            // 
+            // btnAra
+            // 
+            this.btnAra.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnAra.Location = new System.Drawing.Point(180, 30);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(44, 22);
+            this.btnAra.TabIndex = 2;
+            this.btnAra.Text = "ARA";
+            this.btnAra.UseVisualStyleBackColor = true;
+            this.btnAra.Click += new System.EventHandler(this.btnAra_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(258, 461);
+            this.Controls.Add(this.btnAra);
+            this.Controls.Add(this.txtSayi);
+            this.Controls.Add(this.lbSayilar);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbSayilar;
+        private System.Windows.Forms.TextBox txtSayi;
+        private System.Windows.Forms.Button btnAra;
+    }
+}
+
---
+++ listbox random sayi -96/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace listbox_random_sayi__96
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < 20; i++)
+            {
+                lbSayilar.Items.Add(rnd.Next(1,101));
+            }
+        }
+
+        private void btnAra_Click(object sender, EventArgs e)
+        {
+            int aranan = Convert.ToInt32(txtSayi.Text);
+            bool bulundu = false;
+
+            for(int i = 0; i < 20; i++)
+            {
+                int sayi = Convert.ToInt32(lbSayilar.Items[i]);
+
+                if(sayi == aranan)
+                {
+                    bulundu = true;
+                    break; //break komutu döngüden çıkarır.
+                }
+                if (bulundu)
+                {
+                    MessageBox.Show("Aranan sayı bulundu");
+                }
+                else
+                {
+                    MessageBox.Show("Aranan sayı bulunamadı");
+                }
+
+            }
+        }
+    }
+}
